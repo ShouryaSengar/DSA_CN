6a58f2bf36dedd8b3d5dff0a771ad87ac0f09fa7
Shourya <shouryacode2021@gmail.com>
2022-12-22 21:16:34 +0530

    Printing the Tree
    Adding a print function

--- Trees/TreeUse.cpp
+++ Trees/TreeUse.cpp
@@ -2,6 +2,30 @@
 #include "TreeNode.h"
 using namespace std;
 
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL) // Edge Case not a Base Case
+    {
+        return;
+    }
+
+    cout << root->data << ": ";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data;
+        if (i != root->children.size() - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << endl;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]); // No base case required for generic tree
+    }
+}
+
 int main()
 {
     TreeNode<int> *root = new TreeNode<int>(1);
@@ -9,4 +33,6 @@ int main()
     TreeNode<int> *child2 = new TreeNode<int>(3);
     root->children.push_back(child1);
     root->children.push_back(child2);
+    printTree(root);
+    // TODO: delete the tree
 }
