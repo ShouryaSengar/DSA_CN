63aca891df70abb44ce5113cf8637c0593d3331a
Shourya <shouryacode2021@gmail.com>
2022-12-22 20:37:08 +0530

    Tree Creation
    Making a simple tree

---
+++ Trees/TreeUse.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "TreeNode.h"
+using namespace std;
+
+int main()
+{
+    TreeNode<int> *root = new TreeNode<int>(1);
+    TreeNode<int> *child1 = new TreeNode<int>(2);
+    TreeNode<int> *child2 = new TreeNode<int>(3);
+    root->children.push_back(child1);
+    root->children.push_back(child2);
+}
