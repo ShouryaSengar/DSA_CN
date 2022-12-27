9cbfdd0f08d81ad2fa434b485d4dec9d0414fa45
Shourya <shouryacode2021@gmail.com>
2022-12-27 17:43:27 +0530

    Counting Number of TreeNodes

--- Trees/TreeUse.cpp
+++ Trees/TreeUse.cpp
@@ -100,6 +100,16 @@ void printTree(TreeNode<int> *root)
     }
 }
 
+int numNodes(TreeNode<int> *root)
+{
+    int ans = 1;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        ans += numNodes(root->children[i]);
+    }
+    return ans;
+}
+
 int main()
 {
     // TreeNode<int> *root = new TreeNode<int>(1);
