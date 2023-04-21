6e7f133879b280063cbfcde633fd6d9bb52649d6
Shourya Singh Sengar <93751651+ShouryaSengar@users.noreply.github.com>
2023-04-21 18:48:44 +0530

    Find Height of Binary Tree

---
+++ BinaryTrees/BT_Questions/HeightBT.cpp
@@ -0,0 +1,76 @@
+/*
+For a given Binary Tree of integers, find and return the height of the tree.
+Example:
+                        10
+                      /    \
+                    20      30
+                   /   \
+                 40    50
+
+Height of the given tree is 3. 
+Height is defined as the total number of nodes along the longest path from the root to any of the leaf node.
+
+Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The first and the only line of output prints the height of the given binary tree.
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int height(BinaryTreeNode<int> *root) 
+{
+  
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    cout << height(root);
+}
