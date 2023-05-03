8692c66cf082b860bdb6ae4aa718d1e966afde80
Shourya Singh Sengar <93751651+ShouryaSengar@users.noreply.github.com>
2023-05-03 10:33:40 +0530

    Naive Method O(N*N)

--- BinaryTrees/BT_Questions/CheckBalanced.cpp
+++ BinaryTrees/BT_Questions/CheckBalanced.cpp
@@ -1,9 +1,6 @@
 /*
 Given a binary tree, check if it is balanced. Return true if given binary tree is balanced, false otherwise.
-Balanced Binary Tree:
-A empty binary tree or binary tree with zero nodes is always balanced. For a non empty binary tree to be balanced, following conditions must be met:
-1. The left and right subtrees must be balanced.
-2. |hL - hR| <= 1, where hL is the height or depth of left subtree and hR is the height or depth of right subtree.    
+Balanced Binary Tree:   
 Input format:
 The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. 
 If any node does not have a left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.
@@ -68,10 +65,31 @@ BinaryTreeNode<int>* takeInput() {
     }
     return root;
 }
+/*
+A empty binary tree or binary tree with zero nodes is always balanced. For a non empty binary tree to be balanced, 
+following conditions must be met:
+1. The left and right subtrees must be balanced.
+2. |hL - hR| <= 1, where hL is the height or depth of left subtree and hR is the height or depth of right subtree. 
+*/
+int height(BinaryTreeNode<int> *root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 +  max(height(root->left), height(root->right));
+	}
+}
 
 bool isBalanced(BinaryTreeNode<int> *root) {
 	// Write your code here
-	if()
+	if (root == NULL)
+	{
+		return true;
+	}
+	return abs(height(root->left) - height(root->right)) <= 1;
 }
 
 int main() {
